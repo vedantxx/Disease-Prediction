a132afb1c1055dbdbc526ca974eb0f5cd920fe10
vedantjadhav.vj10@gmail.com <Mygithubidved@101>
2023-04-21 17:19:18 +0530

    First Commit

---
+++ lib/model/speciality.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class SpecialityModel{
+
+  String? imgAssetPath;
+  String? speciality;
+  // int? noOfDoctors;
+  Color? backgroundColor;
+  SpecialityModel({
+    this.imgAssetPath,
+    this.speciality,
+    // this.noOfDoctors,
+    this.backgroundColor});
+}
